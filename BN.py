ef8ddc2e814bd4e12353f755016b41fd45aa4566
thinker <thinker1065948474@gmail.com>
2019-10-25 14:02:51 +0800

    fix bugs

--- BN.py
+++ BN.py
@@ -8,7 +8,7 @@ class _GBN(nn.Module):
                      'running_mean', 'running_var', 'num_batches_tracked',
                      'num_features', 'affine']
 
-    def __init__(self, opt, num_features, eps=1e-5, momentum=0.1, affine=True,
+    def __init__(self, opt, num_features, eps=1e-5, momentum=0.01, affine=True,
                  track_running_stats=True):
         super(_GBN, self).__init__()
         self.opt = opt
@@ -48,10 +48,7 @@ class _GBN(nn.Module):
     def forward(self, input):
         self._check_input_dim(input)
         N,C,_,_ = input.size()
-        output,running_mean,running_var = self.g_b_n(input,self.running_mean,self.running_var,self.weight.expand(N,C),self.bias.expand(N,C))
-        if self.training:
-            self.running_mean.data = running_mean.data
-            self.running_var.data = running_var.data
+        output = self.g_b_n(input,self.running_mean,self.running_var,self.weight.expand(N,C),self.bias.expand(N,C))
         return output
 
     def extra_repr(self):
@@ -71,14 +68,14 @@ class _GBN(nn.Module):
         var = torch.var(input,(1,3,4),keepdim=True)
 
         if self.training:
-            running_mean = running_mean*self.momentum + mean*(1-self.momentum)
-            running_var = running_var*self.momentum + var*(1-self.momentum)
+            running_mean.data = running_mean.data*(1-self.momentum) + mean*self.momentum
+            running_var.data = running_var.data*(1-self.momentum) + var*self.momentum
             X_hat = (input-mean)/torch.sqrt(var+self.eps)
         else:
             X_hat = (input-running_mean)/torch.sqrt(running_var+self.eps)
         X_hat = X_hat.view(N,C,H,W)
         output = X_hat*weight.view(N,self.num_features,1,1)+beta.view(N,self.num_features,1,1)
-        return output,running_mean,running_var
+        return output
 
 
 class GBN(_GBN):
@@ -89,7 +86,7 @@ class GBN(_GBN):
 
 class GCBN(_GBN):
     def __init__(self,opt,num_features):
-        super(GCBN,self).__init__(opt,num_features)
+        super(GCBN,self).__init__(opt,num_features,affine=False)
         self.num_features = num_features
         inter_dim = 2*num_features
         self.gamma_mlp = nn.Sequential(
@@ -104,21 +101,9 @@ class GCBN(_GBN):
         )
     def forward(self,input,y):
         self._check_input_dim(input)
-        N,C,_,_ = input.size()
         delta_gamma = self.gamma_mlp(y)
         delta_beta = self.beta_mlp(y)
-        
-        # gamma_cloned = self.weight.clone()
-        # beta_cloned = self.bias.clone()
-        # gamma_cloned = gamma_cloned.view(1,C).expand(N,C)
-        # beta_cloned = beta_cloned.view(1,C).expand(N,C)
-
-        # gamma_cloned = gamma_cloned + delta_gamma
-        # beta_cloned = beta_cloned + delta_beta
-        output,running_mean,running_var = self.g_b_n(input,self.running_mean,self.running_var,delta_gamma,delta_beta)
-        if self.training:
-            self.running_mean.data = running_mean.data
-            self.running_var.data = running_var.data
+        output = self.g_b_n(input,self.running_mean,self.running_var,delta_gamma,delta_beta)
         return output
 
     def _check_input_dim(self, input):
@@ -126,42 +111,42 @@ class GCBN(_GBN):
             raise ValueError('expected 4D input (got {}D input)'
                              .format(input.dim()))
 
-class CBN(nn.Module):
-    def __init__(self,opt,num_features,eps=1e-5):
-        super(CBN,self).__init__()
-        self.num_features = num_features
-        inter_dim = 2*num_features
-        self.eps = eps
-        self.gamma_mlp = nn.Sequential(
-            nn.Linear(128,inter_dim),
-            nn.ReLU(),
-            nn.Linear(inter_dim,num_features)
-        )
-        self.beta_mlp = nn.Sequential(
-            nn.Linear(128,inter_dim),
-            nn.ReLU(),
-            nn.Linear(inter_dim,num_features)
-        )
+# class CBN(nn.Module):
+#     def __init__(self,opt,num_features,eps=1e-5):
+#         super(CBN,self).__init__()
+#         self.num_features = num_features
+#         inter_dim = 2*num_features
+#         self.eps = eps
+#         self.gamma_mlp = nn.Sequential(
+#             nn.Linear(128,inter_dim),
+#             nn.ReLU(),
+#             nn.Linear(inter_dim,num_features)
+#         )
+#         self.beta_mlp = nn.Sequential(
+#             nn.Linear(128,inter_dim),
+#             nn.ReLU(),
+#             nn.Linear(inter_dim,num_features)
+#         )
 
-        # self.weight = Parameter(torch.Tensor(num_features))
-        # self.bias = Parameter(torch.Tensor(num_features))
+#         # self.weight = Parameter(torch.Tensor(num_features))
+#         # self.bias = Parameter(torch.Tensor(num_features))
 
-        self.running_mean = Parameter(torch.Tensor(1,num_features,1,1), requires_grad = False)
-        self.running_var = Parameter(torch.Tensor(1,num_features,1,1), requires_grad = False)
-        self.running_mean.zero_()
-        self.running_var.fill_(1)
+#         self.running_mean = Parameter(torch.Tensor(1,num_features,1,1), requires_grad = False)
+#         self.running_var = Parameter(torch.Tensor(1,num_features,1,1), requires_grad = False)
+#         self.running_mean.zero_()
+#         self.running_var.fill_(1)
 
-    def forward(self,x,y):
-        N,C,H,W = x.size()
-        x_mean = torch.mean(x,(0,2,3),keepdim=True)
-        x_var = torch.var(x,(0,2,3),keepdim=True)
-        gamma = self.gamma_mlp(y)
-        beta = self.bata_mlp(y)
-        if self.training:
-            self.running_mean = 0.99*self.running_mean+0.01*x_mean
-            self.running_var = 0.99*self.running_var+0.01*x_var
-            X_hat = (x-x_mean)/torch.sqrt(x_var+self.eps)
-        else:
-            X_hat = (x-self.running_mean)/torch.sqrt(self.running_var+self.eps)
-        out = X_hat*gamma.view(N,C,1,1)+beta.view(N,C,1,1)
-        return out
+#     def forward(self,x,y):
+#         N,C,H,W = x.size()
+#         x_mean = torch.mean(x,(0,2,3),keepdim=True)
+#         x_var = torch.var(x,(0,2,3),keepdim=True)
+#         gamma = self.gamma_mlp(y)
+#         beta = self.bata_mlp(y)
+#         if self.training:
+#             self.running_mean = 0.99*self.running_mean+0.01*x_mean
+#             self.running_var = 0.99*self.running_var+0.01*x_var
+#             X_hat = (x-x_mean)/torch.sqrt(x_var+self.eps)
+#         else:
+#             X_hat = (x-self.running_mean)/torch.sqrt(self.running_var+self.eps)
+#         out = X_hat*gamma.view(N,C,1,1)+beta.view(N,C,1,1)
+#         return out
